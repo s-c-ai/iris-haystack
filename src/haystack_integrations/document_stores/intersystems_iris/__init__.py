a475ce0c0d6a86e69fa4330ea04b65bfd9c7fc4b
Pedro Henrique <pedro@s-c.ai>
2026-04-09 10:58:39 -0300

    fix: fixing import error

--- src/haystack_integrations/document_stores/intersystems_iris/__init__.py
+++ src/haystack_integrations/document_stores/intersystems_iris/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2026-present Pedro Henrique <pedro@s-c.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-from haystack_integrations.document_stores.iris.document_store import IRISDocumentStore
+from haystack_integrations.document_stores.intersystems_iris import IRISDocumentStore
 
 __all__ = ["IRISDocumentStore"]
