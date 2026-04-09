bd5d23711db0173397e0f8c4e6e88ce01087a0bf
Pedro Henrique <pedrosilv1512@gmail.com>
2026-04-09 13:05:30 -0300

    refac: update import paths in __init__.py

--- src/haystack_integrations/document_stores/intersystems_iris/__init__.py
+++ src/haystack_integrations/document_stores/intersystems_iris/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2026-present Pedro Henrique <pedro@s-c.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-from haystack_integrations.document_stores.intersystems_iris import IRISDocumentStore
+from .document_store import IRISDocumentStore
 
 __all__ = ["IRISDocumentStore"]
